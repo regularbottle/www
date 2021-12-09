d58a54aa6f5bdc0e1a8ec9b0e15f74ec5425ed2b
regularbottle <calin.furculita@gmail.com>
2021-12-09 12:10:21 +0100

    added limit to array

--- php/lezione1/esempio.php
+++ php/lezione1/esempio.php
@@ -3,25 +3,20 @@
 
 <body>
   <?php
-  echo 'Prima pagina<br>';
-  echo 'Oggi è il ', date('Y/m/d');
-  echo '<br>';
-  $data = date('H:i:s');
-  echo 'Daniele dice: "Pausa" alle ', $data, '!<br>';
-
+  $limite = random_int(1, 100);
   $numeri = range(1, 1000);
   shuffle($numeri);
-  $numeri = array_slice($numeri, 0, 10);
-  
+  $numeri = array_slice($numeri, 0, $limite + 1);
+
   var_dump($numeri);
 
-  $ppiuccolo = $numeri['0'];
-  for ($i = 0; $i < 9; ++$i) {
-      if ($ppiuccolo > $numeri[$i + 1]) {
-          $ppiuccolo = $numeri[$i + 1];
+  $ppiccolo = $numeri['0'];
+  for ($i = 0; $i < $limite; ++$i) {
+      if ($ppiccolo > $numeri[$i + 1]) {
+          $ppiccolo = $numeri[$i + 1];
       }
   }
-  echo "<br>Il numero piu' piccolo e' ", $ppiuccolo;
+  echo "<br>Il numero piu' piccolo e' ", $ppiccolo;
   ?>
 </body>
 </html>
