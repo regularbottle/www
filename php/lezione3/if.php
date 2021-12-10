5f6904d412b05d1ad6a754099ddf62c8bc343948
regularbottle <calin.furculita@gmail.com>
2021-12-10 10:35:17 +0100

    prima pausa

--- php/lezione3/if.php
+++ php/lezione3/if.php
@@ -6,6 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP - Lezione 3</title>
+    <style>
+        .container {
+            display: flex;
+            justify-content: flex-start;
+        }
+        @media all and (max-width: 800px) {
+            .container {
+                justify-content: space-around;
+            }
+        }
+
+        @media all and (max-width: 500px) {
+            .container {
+                flex-direction: column;
+            }
+        }
+
+        .item {
+            padding-left: 5%;
+            padding-right: 5%;
+        }
+    </style>
 </head>
 <body>
 <div class="container" id="lezione3">
@@ -21,6 +43,26 @@
         } else {
             echo "$a minore $b";
         }
+        echo "<br>fine";
+
+        echo "<hr>";
+        //date due var a e b fare in modo che a sia maggiore di b
+        try {
+            $a = random_int(1, 100);
+        } catch (Exception $e) {
+        }
+        try {
+            $b = random_int(1, 100);
+        } catch (Exception $e) {
+        }
+        echo "I valori iniziali sono <ol type=\"a\"><li>$a</li><li>$b</li></ol>";
+        $c = 0;
+        if ($a<$b) {
+            $c=$b;
+            $b=$a;
+            $a=$c;
+        }
+        echo "Ora i valori sono <ol type=\"a\"><li>$a</li><li>$b</li></ol>";
         ?>
     </div>
 </div>
