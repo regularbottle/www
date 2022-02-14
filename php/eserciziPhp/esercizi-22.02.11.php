49ded6bb9756dae134f13b737326d682b0b46017
Regular <calin.furculita@gmail.com>
2022-02-14 16:24:45 +0100

    finished ex

--- php/eserciziPhp/esercizi-22.02.11.php
+++ php/eserciziPhp/esercizi-22.02.11.php
@@ -1,14 +1,11 @@
 <?php
-
 class Corso
 {
-    const FINANZIAMENTO = "Il corso è finanziato con i fondi dell'Unione Europea";
-    private
-        $voti = [], //elenco dei voti degli studenti: 1 voto per ogni studente iscritto (quindi il numero dei voti coincide con il numero degli studenti)
-        $nome, //nome corso
-        $posti, //n. di posti disponibili
-        $valutazione;  //valutazione assegnata al corso dagli studenti
-
+    const FINANZIAMENTO = "Il corso è finanziato con i fondi del Unione Europea";
+    private array $voti = []; //elenco dei voti degli studenti: 1 voto per ogni studente iscritto (quindi il numero dei voti coincide con il numero degli studenti)
+    private string $nome; //nome corso
+    private int $posti; //n. di posti disponibili
+    private int $valutazione;  //valutazione assegnata al corso dagli studenti
 
     function __construct($nome, $posti)
     {
@@ -21,29 +18,29 @@ class Corso
         $this->voti[] = $voto;
     }
 
-    function getVoto()
+    function getVoto(): array
     {
         return $this->voti;
     }
 
-    function getPosti()
+    function getPosti(): int
     {
         return $this->posti;
     }
 
-    function getNome()
+    function getNome(): string
     {
         return $this->nome;
+    }
+
+    function getValutazione(): int
+    {
+        return $this->valutazione;
     }
 
     function setValutazione($valutazione)
     {
         $this->valutazione = $valutazione;
-    }
-
-    function getValutazione()
-    {
-        return $this->valutazione;
     }
 
     function getStudenti(): int
@@ -56,7 +53,7 @@ class Corso
      */
     function completo(): bool
     {
-        return count($this->voti) == $this->posti;
+        return count($this->voti) >= $this->posti;
     }
 
     /**
@@ -70,39 +67,41 @@ class Corso
 
 $ap = new Corso('Analista Programmatore', 12);
 $ifts = new Corso('IFTS', 20);
+$plumbing = new Corso('Plumbing', 223);
+$piping = new Corso('Piping', 11);
+$pizzaiolo = new Corso('Pizzaiolo', 666);
 
 //1. assegnare i voti random a ogni corso (in numero non superiore al n. di posti)
 //quanti voti assegnare a ogni corso? Assegnare un numero random
-
+$corsi = [$ap, $ifts, $plumbing, $piping, $pizzaiolo]; //Un array di oggetti!
 try {
-    for ($i = 0; $i < random_int(1, $ap->getPosti()); $i++) {
-        $ap->setVoto(random_int(1, 30));
+    foreach ($corsi as $key => $value) {
+        for ($i = 0; $i < random_int(1, $value->getPosti()); $i++) {
+            $value->setVoto(random_int(1, 30));
+        }
+        $value->setValutazione(random_int(1, 30));
     }
-    $ap->setValutazione(random_int(1, 30));
-    for ($i = 0; $i < random_int(1, $ifts->getPosti()); $i++) {
-        $ifts->setVoto(random_int(1, 30));
-    }
-
-    $ifts->setValutazione(random_int(1, 30));
 } catch (Exception $e) {
+    echo "Errore inaspettato";
 }
-
-$corsi = [$ap, $ifts]; //un array di oggetti!
-
 //Lavorare sul vettore $corsi
-//2. elencare i nomi dei corsi
-//3. dopo ogni nome inserire sempre il finanziamento (costante definita nella classe)
+//2. Elencare i nomi dei corsi
+//3. Dopo ogni nome inserire sempre il finanziamento (costante definita nella classe)
 foreach ($corsi as $key => $value) {
     echo $value->getNome() . " " . $value::FINANZIAMENTO . "<br>";
 }
-//4. qual è il corso con la valutazione maggiore?
-if ($corsi[0]->getValutazione() > $corsi[1]->getValutazione()) {
-    echo "Il corso con la Valutazione maggiore è stato: " . $corsi[0]->getNome() . " con un voto di: " . $corsi[0]->getValutazione() . "<br>";
-} else {
-    echo "Il corso con la Valutazione maggiore è stato: " . $corsi[1]->getNome() . " con un voto di: " . $corsi[1]->getValutazione() . "<br>";
-}
-//5. qual è la partecipazione media dei corsi?
+//4. Qual è il corso con la valutazione maggiore?
+$massimo = [];
 foreach ($corsi as $key => $value) {
-    echo "Con " . $value->getStudenti() . " studenti e " . $value->getPosti() . " posti, la percentuale di partecipazione e' stata del: " . number_format($value->partecipazione(), 2) . "%" . "<br>";
+    $massimo[] = [$value->getValutazione() , $value->getNome() ];
 }
-
+rsort($massimo);
+echo "Il corso con la Valutazione maggiore è stato: ";
+echo $massimo[0][1] . " con un voto di: " . $massimo[0][0] . "<br>";
+//5. Qual è la partecipazione media dei corsi?
+foreach ($corsi as $key => $value) {
+    echo "Con " . $value->getStudenti() . " studenti e " . $value->getPosti()
+                . " posti, la percentuale di partecipazione al corso "
+                . $value->getNome() . " e' stata del: "
+                . number_format($value->partecipazione(), 2) . "%" . "<br>";
+}
