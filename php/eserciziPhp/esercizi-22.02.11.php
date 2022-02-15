c8a5a4131574b0261b052afd6762d4f884513a0d
regularbottle <calin.furculita@gmail.com>
2022-02-15 15:12:20 +0100

    garbage collection

--- php/eserciziPhp/esercizi-22.02.11.php
+++ php/eserciziPhp/esercizi-22.02.11.php
@@ -90,14 +90,21 @@ try {
 foreach ($corsi as $key => $value) {
     echo $value->getNome() . " " . $value::FINANZIAMENTO . "<br>";
 }
+
+echo "<hr>";
+
 //4. Qual è il corso con la valutazione maggiore?
 $massimo = [];
 foreach ($corsi as $key => $value) {
     $massimo[] = [$value->getValutazione() , $value->getNome()];
+    echo  $value->getNome() . " : " .  $value->getValutazione() . "<br>";
 }
 rsort($massimo);
-echo "Il corso con la Valutazione maggiore è stato: ";
+echo "<br>Il corso con la Valutazione maggiore è stato ";
 echo $massimo[0][1] . " con un voto di: " . $massimo[0][0] . "<br>";
+
+echo "<hr>";
+
 //5. Qual è la partecipazione media dei corsi?
 foreach ($corsi as $key => $value) {
     echo "Con " . $value->getStudenti() . " studenti e " . $value->getPosti()
