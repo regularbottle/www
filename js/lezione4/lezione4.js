717d856b40b443a1115cbb55db737f343d48a5fe
regularbottle <calin.furculita@gmail.com>
2022-02-23 10:56:24 +0100

    wtf

--- js/lezione4/lezione4.js
+++ js/lezione4/lezione4.js
@@ -1,22 +1,24 @@
-function Calcolatrice() {
-    this.leggi = function (numero1, numero2) {
-        this.numero1 = numero1;
-        this.numero2 = numero2;
-    }
-    this.somma = function () {
-        return this.numero1 + this.numero2;
-    }
-    this.moltiplica = function () {
-        return this.numero1 * this.numero2;
-    }
-    this.dividi = function () {
-        return this.numero1 / this.numero2;
-    }
-}
+// function Calcolatrice(numero1, numero2) {
+//     this.numero1 = numero1;
+//     this.numero2 = numero2;
+//     this.somma = function () {
+//         return this.numero1 + this.numero2;
+//     }
+//     this.moltiplica = function () {
+//         return this.numero1 * this.numero2;
+//     }
+//     this.dividi = function () {
+//         if (this.numero2 !== 0) {
+//             return this.numero1 / this.numero2;
+//         } else {
+//             console.log("Divisore uguale a 0, impossibile dividere")
+//         }
+//     }
+// }
+//
+// let calcolatrice = new Calcolatrice(10, -1.2);
+//
+// console.log(calcolatrice.somma());
+// console.log(calcolatrice.moltiplica());
+// console.log(calcolatrice.dividi());
 
-let calcolatrice = new Calcolatrice();
-
-calcolatrice.leggi(10, 10);
-console.log(calcolatrice.somma());
-console.log(calcolatrice.moltiplica());
-console.log(calcolatrice.dividi());
